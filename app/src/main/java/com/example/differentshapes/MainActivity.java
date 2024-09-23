307b3f6d86c40979b4d3ef3bab2923b614281c5b
Vishwa Tej <ixsaac707@gmail.com>
2024-09-23 19:14:15 +0530

    comments removed

--- app/src/main/java/com/example/differentshapes/MainActivity.java
+++ app/src/main/java/com/example/differentshapes/MainActivity.java
@@ -15,30 +15,23 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//Creating a Bitmap
-        Bitmap bg = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);
-//Setting the Bitmap as background for the ImageView
+        Bitmap bg = Bitmap.createBitmap(720, 1280, Bitmap.Config.ARGB_8888);//Setting the Bitmap as background for the ImageView
         ImageView i = (ImageView) findViewById(R.id.imageView);
         i.setBackgroundDrawable(new BitmapDrawable(bg));
-//Creating the Canvas Object
         Canvas canvas = new Canvas(bg);
-//Creating the Paint Object and set its color & TextSize
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
         paint.setTextSize(50);
-//To draw a Rectangle
+
         canvas.drawText("Rectangle", 420, 150, paint);
         canvas.drawRect(400, 200, 650, 700, paint);
         paint.setColor(Color.GREEN);
-//To draw a Circle
         canvas.drawText("Circle", 120, 150, paint);
         canvas.drawCircle(200, 350, 150, paint);
         paint.setColor(Color.MAGENTA);
-//To draw a Square
         canvas.drawText("Square", 120, 800, paint);
         canvas.drawRect(50, 850, 350, 1150, paint);
         paint.setColor(Color.BLACK);
-//To draw a Line
         canvas.drawText("Line", 480, 800, paint);
         canvas.drawLine(520, 850, 520, 1150, paint);
     }
